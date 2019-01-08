2eaf72d1cf5a58ebbf4abbe80977f646fd475011
CheezItMan <chris@adadevelopersacademy.org>
2019-01-07 17:06:42 -0800

    render list of classrooms

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,4 +1,11 @@
 class ClassesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    render json: {
+      ok: true,
+      classes: Classroom.all
+    }, status: :ok
+
   end
 end
