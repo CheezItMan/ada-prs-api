4d07ce5a6fbf48014c451a114b1a71f6f7f13c0b
CheezItMan <chris@adadevelopersacademy.org>
2019-01-07 17:05:42 -0800

    helper methods to handle authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  def current_user
+    token = request.headers['authorization']
+
+    if token.nil?
+      return nil
+    end
+
+    payload = JsonWebToken.decode(token)
+    return @current_user ||= User.find_by_login(payload[0]['sub'])
+  end
+
+  def logged_in?
+    return current_user != nil
+  end
+
+  def authenticate_user!
+    head :unauthorized unless logged_in?
+  end
 end
