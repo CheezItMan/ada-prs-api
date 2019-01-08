e88e2d98b1c0ef572de4629f7c96b179391f5c2e
CheezItMan <chris@adadevelopersacademy.org>
2019-01-08 08:11:49 -0800

    added repo model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Repo.create(repo_url: "https://github.com/ada-c10/inspiration-board", individual: true)
+
+Repo.create(repo_url: "https://github.com/Ada-C10/video-store-consumer", individual: false)
+
