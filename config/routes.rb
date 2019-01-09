7792a5e10e3dfc8801adff40016100f3c2c82951
CheezItMan <chris@adadevelopersacademy.org>
2019-01-09 13:58:21 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index]
   resources :repos, only: [:index]
-  resources :classes, only: [:index, :create, :update]
+  resources :classes, only: [:index, :create, :update, :destroy] do
+    resources :repos, only: [:index]
+  end
   resources :prs, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
