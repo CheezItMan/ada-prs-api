45e939fbf55db9498e484303889f2bb2a8a38122
CheezItMan <chris@adadevelopersacademy.org>
2019-01-08 17:14:40 -0800

    add create and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index]
   resources :repos, only: [:index]
-  resources :classes, only: [:index]
+  resources :classes, only: [:index, :create, :update]
   resources :prs, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
