fb988af777636881ce588be5ad9c490464bf8060
CheezItMan <chris@adadevelopersacademy.org>
2019-01-09 16:46:33 -0800

    returning json for repo list

--- app/controllers/prs_controller.rb
+++ app/controllers/prs_controller.rb
@@ -1,4 +1,10 @@
 class PrsController < ApplicationController
   def index
+    classroom = Classroom.find_by(id: params[:id])
+
+    render json: {
+      ok: true,
+      repos: classroom.repos
+    }, status: :ok
   end
 end
