2cae4dc7d4ee97c05b1c30d970965e2feba0a4f1
CheezItMan <chris@adadevelopersacademy.org>
2019-01-09 16:45:21 -0800

    add save token in model

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticationController < ApplicationController
-  def github
+  def github    
     authenticator = Authenticator.new
     user_info = authenticator.github(params[:code])
 
@@ -8,17 +8,22 @@ class AuthenticationController < ApplicationController
     avatar_url = user_info[:avatar_url]
     email = user_info[:email]
     uid = user_info[:id]
+    access_token = user_info[:access_token]
 
     # Generate token
     token = JsonWebToken.encode(login)
     # create a user if it doesn't exist
-    User.where(uid: uid).first_or_create!(
+    user = User.where(uid: uid).first_or_create!(
       name: name,
       login: login,
       avatar_url: avatar_url,
       email: email,
       uid: uid
     )
+ 
+    # Save the current access token in the model to use act on their behalf in github
+    user.current_access_token = access_token
+    user.save
 
     # and redirect to the client app
     redirect_to "#{issuer}/auth?token=#{token}"
