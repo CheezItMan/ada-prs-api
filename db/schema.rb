500baaf5da82e646826cb2c520f7def87a6b047b
CheezItMan <chris@adadevelopersacademy.org>
2019-01-09 16:47:32 -0800

    added access token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_211720) do
+ActiveRecord::Schema.define(version: 2019_01_09_224645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_211720) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "uid"
+    t.string "current_access_token"
   end
 
   add_foreign_key "classroom_repos", "classrooms"
