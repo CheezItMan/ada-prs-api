1d2aa38222f3063f32725e7457be2f6736389b0e
CheezItMan <chris@adadevelopersacademy.org>
2019-01-14 16:43:57 -0800

    copied v1 methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,33 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :login, presence: true
 
-  validates :email, presence: true, format: { with: /@/, message: 'must include a @' }
+  has_and_belongs_to_many :classrooms
+
+  validates :email, presence: true, format: {with: /@/, message: "must include a @"}
 
   def admin?
     return self.admin
+  end
+
+  def pr_id
+    self.pr_url.split("/").last
+  end
+
+  def update_group(attrs)
+    if repo.individual?
+      update(attrs)
+    else
+      Submission.grouped_with(self).update_all(attrs)
+    end
+  end
+
+  def student_names
+    if repo.individual?
+      student.name
+    else
+      Submission.grouped_with(self).map do |sub|
+        sub.student.name
+      end.join(" & ")
+    end
   end
 end
