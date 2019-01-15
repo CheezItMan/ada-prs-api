faf2bcee1e56a4302fbbdefd0adab1b541b99fbd
CheezItMan <chris@adadevelopersacademy.org>
2019-01-14 16:45:53 -0800

    remove old model

--- app/models/classroom_repo.rb
+++
@@ -1,4 +0,0 @@
-class ClassroomRepo < ApplicationRecord
-  belongs_to :classroom
-  belongs_to :repo
-end
--- test/models/classroom_repo_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe ClassroomRepo do
-  let(:classroom_repo) { ClassroomRepo.new }
-
-  it "must be valid" do
-    value(classroom_repo).must_be :valid?
-  end
-end
