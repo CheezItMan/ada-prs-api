68938072a56b28c08f1f2d8a56cac9bdefde435d
CheezItMan <chris@adadevelopersacademy.org>
2019-01-14 16:42:50 -0800

    copied repos methods from v1

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,8 +1,38 @@
 class Repo < ApplicationRecord
-
-  has_many :classroomrepos
-  has_many :classrooms, through: :classroomrepos
+  has_and_belongs_to_many :classrooms
 
   validates :repo_url, presence: true
 
+  BASE_GITHUB_URL = "https://github.com/"
+
+  default_scope { order(created_at: :desc) }
+
+  def name
+    name = self.repo_url.split("/")[1]
+    return name ? name : self.repo_url
+  end
+
+  def pulls_url
+    full_repo_url + "/pulls"
+  end
+
+  def full_repo_url
+    BASE_GITHUB_URL + self.repo_url
+  end
+
+  def branch_url
+    full_repo_url + "/blob/master/"
+  end
+
+  def feedback_template_url
+    branch_url + "feedback.md"
+  end
+
+  def pr_template_url
+    branch_url + ".github/PULL_REQUEST_TEMPLATE"
+  end
+
+  def get_github_prs
+
+  end
 end
