f67bbb24ea2270512bd189c69f220e04b1d66d9c
CheezItMan <chris@adadevelopersacademy.org>
2019-01-14 16:33:06 -0800

    add index action to repos controller

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,7 +1,42 @@
-class ReposController < ApplicationController
-  def index
-    
+# frozen_string_literal: true
 
-    authorize! :index, Repos
+class ReposController < ApplicationController
+  before_action :find_classroom, only: [:index, :show]
+
+  def index
+    authorize! :index, Repo
+
+    ap "CLASSROOM APP"
+    ap @classroom
+    puts params
+
+    if @classroom
+      @repos = @classroom.repos
+    else
+      @repos = Repo.all
+    end
+
+    if @repos
+      render json: {
+        ok: true,
+        repos: @repos,
+      }, status: :ok
+    else
+      render json: {
+        ok: false,
+        message: "Not found",
+      }, status: :not_found
+    end
+  end
+
+  def show
+    repo = Repo.find_by id: params[:id]
+    students = @classroom.students
+  end
+
+  private
+
+  def find_classroom
+    @classroom = Classroom.find_by(id: params[:class_id])
   end
 end
